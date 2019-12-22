db623639c9de6d286da0a08c9ff6adf9ac2ec41b
gaofer <260418437@qq.com>
2019-12-22 09:28:01 +0800

    修改了话题表，实现话题添加功能，增加查看话题html格式的内容

--- src/main/java/com/hbgc/loudbbs/loudbbs/mapper/TTopicMapper.java
+++ src/main/java/com/hbgc/loudbbs/loudbbs/mapper/TTopicMapper.java
@@ -17,7 +17,7 @@ import java.util.List;
  */
 public interface TTopicMapper extends BaseMapper<TTopic> {
 
-    @Select("SELECT t_topic.tid,t_topic.topicname,t_topic.heat,t_topic.mainbody,t_topic.createtime,t_topic.updatetime,t_users.username,t_topic.uid,t_category.cname FROM t_topic INNER JOIN t_users ON t_users.uid = t_topic.uid INNER JOIN t_category ON t_category.cid = t_topic.cid WHERE t_topic.tid = #{tid}")
+    @Select("SELECT t_topic.tid,t_topic.topicname,t_topic.heat,t_topic.htmlmainbody,t_topic.mainbody,t_topic.createtime,t_topic.updatetime,t_users.username,t_topic.uid,t_category.cname FROM t_topic INNER JOIN t_users ON t_users.uid = t_topic.uid INNER JOIN t_category ON t_category.cid = t_topic.cid WHERE t_topic.tid = #{tid}")
     List<TTopicVO> getTopicDetalVO(int tid);
 
     @Select("SELECT t_topic.tid,t_topic.topicname,t_topic.heat,t_topic.mainbody,t_topic.createtime,t_topic.updatetime,t_users.username,t_topic.uid,t_category.cname FROM t_topic INNER JOIN t_users ON t_users.uid = t_topic.uid INNER JOIN t_category ON t_category.cid = t_topic.cid")
